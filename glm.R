f89bb9225eb94ab7fca8686a7f44e0d9ad746cfa
YunSung Kang <kkangyun0511@naver.com>
2022-05-09 15:26:30 +0900

    Revise glm

--- glm.R
+++ glm.R
@@ -1,35 +1,45 @@
 ###함수 설치
 library(dplyr)
-###데이터 불러오기a<-read.csv("C:/Users/kkang/Desktop/crx.data.data",sep=",")
-###????데이터 구조 및 구성 확인(data)
+###데이터 불러오기
+a<-read.csv("C:/use R/a/crx.data.data",sep=",")
+###데이터 구조 및 구성 확인(data)
 dim(data)
 names(data)
 table(data)
 boxplot(data)
-###????데이터 뽑기a.s=data[,c(2,3,8,9,11,14,15)]
+###데이터 뽑기
+data.s=data[,c(2,3,8,9,11,14,15)]
 str(data.s)
-### ???데이터 구조 바꾸기s.factor(data.s$t)
+###데이터 구조 바꾸기s.factor(data.s$t)
 z=as.numeric(data.s$X30.83)
 m=as.numeric((data.s$X00202))
 x=(data.s[,c(-1,-4,-6)])
 data.y.s=cbind(x,y,z,m)
-###????결측치 제거.s=na.omit(data.y.s)
+###결측치 제거.s=na.omit(data.y.s)
 dim(data.na.s)
+is.na(data.na.s)
 665*(7/10)
-###train, test ?з?
-s=s뽑기기e(1:665,465)
+###train, test 뽑기
+s=sample(1:665,465)
 train=data.na.s[s,]
 test=data.na.s[-s,]
-###
+###train 신용등급
 (train)
 str(train)
 data.g=glm(y~., family=binomial, data=train)
 summary(data.g)
-train$X0
 
 p=exp(-2.1623061+0.0764472*train$X0 +0.3020987*train$X1.25+0.4010907*train$X01+0.0003696*train$X0.1)/(1+exp(-2.1623061+0.0764472*train$X0 +0.3020987*train$X1.25+0.4010907*train$X01+0.0003696*train$X0.1))
-
-
-
-train.test.pre=predict(data.g,test[])
-
+credit<-as.numeric(p>0.5)
+str(credit)
+data.f<-train+train$credit
+train[,c(credit)]
+rm(data.f)
+data.f<-data.frame(train,credit.1=c(credit))
+data.f
+###test 신용등급급
+p.t=exp(-2.1623061+0.0764472*test$X0 +0.3020987*test$X1.25+0.4010907*test$X01+0.0003696*test$X0.1)/(1+exp(-2.1623061+0.0764472*test$X0 +0.3020987*test$X1.25+0.4010907*test$X01+0.0003696*test$X0.1))
+credit.1<-as.numeric(p.t>0.5)
+credit.1
+data.ff<-data.frame(test,credt.t=c(credit.1))
+data.ff
